102c1ed81d8bb586a6089633c6f21ac7facf6ff9
towhidulislamalif <towhidulislamalif@gmail.com>
2022-10-25 11:18:18 +0600

    website name done Alhamdulillah

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCJ9_nO9YIqK28EfpZtaIMvHpFiYm_Fy8E',
+  authDomain: 'codecamp-8f4e0.firebaseapp.com',
+  projectId: 'codecamp-8f4e0',
+  storageBucket: 'codecamp-8f4e0.appspot.com',
+  messagingSenderId: '605748931404',
+  appId: '1:605748931404:web:9d2ae32db0327abb577de9',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
