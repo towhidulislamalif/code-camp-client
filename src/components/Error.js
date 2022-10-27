27891006ce50d0cca3f34f0e568d247cb90fa231
towhidulislamalif <towhidulislamalif@gmail.com>
2022-10-27 15:59:33 +0600

    error page done

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,7 +5,7 @@ function Error() {
   const error = useRouteError();
 
   return (
-    <section className="flex items-center h-full p-16 bg-gray-900 text-gray-100">
+    <section className="min-h-screen flex items-center h-full p-16 bg-gray-900 text-gray-100">
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
         <div className="max-w-md text-center">
           <h2 className="mb-8 font-extrabold text-9xl text-gray-600">
